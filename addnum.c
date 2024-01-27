ea142992c978848dd81600cbd8ee471dc963a1fa
Akash <aakashsoni8756@gmail.com>
2024-01-27 14:41:17 +0530

    Area of Circle program created

--- addnum.c
+++ addnum.c
@@ -1,10 +1,10 @@
-#include<stdio.h>
-int main(){
-    int a,b,c;
+#include <stdio.h>
+int main()
+{
+    int a, b, c;
     printf("enter two no.");
-    scanf("%d%d",&a,&b);
-    c=a+b;
-    printf("the sum of %d and %d is %d",a,b,c);
-     return 0;
-
+    scanf("%d%d", &a, &b);
+    c = a + b;
+    printf("the sum of %d and %d is %d", a, b, c);
+    return 0;
 }
