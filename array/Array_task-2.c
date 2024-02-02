f411d4d703a3ed5af95dbe6052483c8013011184
Akash <aakashsoni8756@gmail.com>
2024-02-02 23:00:58 +0530

    Array task#2

--- array/Array_task-2.c
+++ array/Array_task-2.c
@@ -2,33 +2,28 @@
 
 void insertionOfElements(int arr[], int size);
 void printArrayElements(int arr[], int size);
+void insertElementAtPosition(int arr[], int size);
 
 int main()
 {
     int size;
     int position, insertedElement;
     int arr[100];
+
     printf("How many Elements Do you want to enter in the Array? :");
     scanf("%d", &size);
+
     insertionOfElements(arr, size);
+
     printArrayElements(arr, size);
-    printf("\nThe value of Element Which do you want to enter?: ");
-    scanf("%d", &insertedElement);
-    printf("\nAt which Position Do you want to enter the Elements in the Array?: ");
-    scanf("%d", &position);
-    for (int i = size - 1; i >= position; i--)
-    {
-        arr[i + 1] = arr[i];
-    }
-    arr[position] = insertedElement;
-    printf("The Updated Array : \n");
-    for (int i = 0; i <= size; i++)
-    {
-        printf("%d ", arr[i]);
-    }
+
+    insertElementAtPosition(arr, size);
+
+    printArrayElements(arr, size + 1);
 
     return 0;
 }
+
 void insertionOfElements(int arr[], int size)
 {
     for (int i = 0; i < size; i++)
@@ -44,4 +39,19 @@ void printArrayElements(int arr[], int size)
     {
         printf("%d ", arr[i]);
     }
+}
+void insertElementAtPosition(int arr[], int size)
+{
+    int i, insertedElement, position;
+    size++;
+
+    printf("\nThe value of Element Which do you want to enter?: ");
+    scanf("%d", &insertedElement);
+
+    printf("\nAt which Position Do you want to insert the Elements in the Array?: ");
+    scanf("%d", &position);
+
+    for (i = size - 1; i >= position; i--)
+        arr[i] = arr[i - 1];
+    arr[position - 1] = insertedElement;
 }
