f2a39eb4f478f289f5c8f145afe6f12c70ba0fa9
Akash Soni <157835381+aakash-sony@users.noreply.github.com>
2024-02-03 21:23:37 +0530

    Array programs of Searching Element (#23)
    * Array Program Of Searching Element

---
+++ array/SearchElement.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+
+void searchOfElement(int arr[], int size);
+
+int main()
+{
+    int size;
+    int arr[100];
+    printf("How many elements do you want to Enter in the Array : ");
+    scanf("%d", &size);
+
+    for (int i = 0; i < size; i++)
+    {
+        printf("Enter the %d Element : ", i + 1);
+        scanf("%d", &arr[i]);
+    }
+
+    printf("\nThe Elements that you have entered are : ");
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    searchOfElement(arr, size);
+
+    return 0;
+}
+
+void searchOfElement(int arr[], int size)
+{
+    int element,i;
+    printf("\nWhich Elements do you want to search? : ");
+    scanf("%d", &element);
+
+    for (i = 0; i < size; i++)
+    {
+        if (arr[i]==element)
+        {
+            printf("Number is available ");
+            break;
+        }
+    }
+    if (arr[i]!=element)
+    printf("Number is not available");
+}
