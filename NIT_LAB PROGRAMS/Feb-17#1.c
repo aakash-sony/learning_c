f1970b634ff4764ce73044323ceb966e4e02effd
Akash <aakashsoni8756@gmail.com>
2024-02-19 21:21:01 +0530

    NIT Lab Program

---
+++ NIT_LAB PROGRAMS/Feb-17#1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+
+{
+    int a,b;
+    char c;
+    printf("Please enter two number: ");
+    scanf("%d %d",&a,&b);
+
+    printf("Please select any arithmatic symbol(+,-,*,/): ");
+    scanf(" %c",&c);
+
+    (c=='+')&&printf("%d",a+b);
+    (c=='-')&&printf("%d",a-b);
+    (c=='*')&&printf("%d",a*b);
+    (c=='/')&&printf("%d",a/b);
+
+    return 0;
+    
+    
+}
