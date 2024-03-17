fc0b5a36f564193616e8f8d05469d39964cddccc
Akash <aakashsoni8756@gmail.com>
2024-03-17 18:12:28 +0530

    Pattern style -5

---
+++ Patterns/Pattern5.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+
+int main()
+{
+    int i,j,rows,space;
+
+    printf("Enter the value of rows: ");
+    scanf("%d",&rows);
+
+    for(i=1;i<=rows;i++)
+    {
+        for(space=1;space<=(rows-i);space++)
+        {
+            printf(" ");
+        }
+        for(j=1;j<=i;j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
