4615e8c72eed2e74719acd0441c63aa13c58b0c3
Akash Soni <157835381+aakash-sony@users.noreply.github.com>
2024-04-02 19:52:22 +0530

    Student Marksheet (#268)
    * Marksheet Of Student

--- Patterns/Pattern4.c
+++ Patterns/Pattern4.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+
 int main()
 {
  int i,j,rows;
@@ -6,6 +7,7 @@ int main()
    printf("Enter the number of rows: ");
     scanf("%d",&rows);
    for(i=1;i<=rows;i++)
+
 
     {
         for(j=1;j<=rows;j++)
